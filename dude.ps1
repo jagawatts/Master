8307dbea8115eb40448916553173fe5dde7542c3
Jaga <109644281+jagawatts@users.noreply.github.com>
2024-08-07 05:07:28 -0400

    Update dude.ps1

--- dude.ps1
+++ dude.ps1
@@ -1,1 +1,5 @@
-Write-Host "Hello,World!"
+# Create a WSH Shell object
+$wshell = New-Object -ComObject Wscript.Shell
+
+# Display the pop-up notification
+$wshell.Popup("Your notification message here", 0, "Notification Title", 0x0)
